6c5c75ee2a39f37e21f84eb9799762dcd27c045b
Emre <emrecan19o7@hotmail.com>
2022-12-07 21:20:45 +0300

    Ders örnek

---
+++ OgrenciWebService/src/test/java/tr/edu/medipol/yova/OgrenciWebService/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yova.OgrenciWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
